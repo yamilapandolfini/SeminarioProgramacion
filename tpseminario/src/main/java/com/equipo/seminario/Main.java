e1161c952e4ca9069374dac65a93ea053b478f03
Nandolorian <83517582+Nandolorian@users.noreply.github.com>
2022-09-04 19:49:35 -0300

    gitignore y pruebas con main.jva

--- tpseminario/src/main/java/com/equipo/seminario/Main.java
+++ tpseminario/src/main/java/com/equipo/seminario/Main.java
@@ -8,24 +8,6 @@ import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
-//public class Main extends Application {
-//
-//    @Override
-//    public void start(Stage stage) {
-//        String javaVersion = System.getProperty("java.version");
-//        String javafxVersion = System.getProperty("javafx.version");
-//        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-//        Scene scene = new Scene(new StackPane(l), 640, 480);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-//    public static void main(String[] args) {
-//        launch();
-//    }
-//
-//}
-
 public class Main extends Application {
 
     @Override
