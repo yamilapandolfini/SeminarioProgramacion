29e395442b5ede45ffdd80c7af04b97ed52b428a
Nandolorian <83517582+Nandolorian@users.noreply.github.com>
2022-09-05 17:19:59 -0300

    Pantallas iniciales

--- tpseminario/src/main/java/com/equipo/seminario/Main.java
+++ tpseminario/src/main/java/com/equipo/seminario/Main.java
@@ -1,22 +1,21 @@
 package com.equipo.seminario;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
 public class Main extends Application {
 
+	
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
-        
         Scene scene = new Scene(root);
-        stage.setTitle("Appointment Manager");
-        
+        stage.setTitle("Administración de Turnos");
         stage.setScene(scene);
         stage.show();
     }
